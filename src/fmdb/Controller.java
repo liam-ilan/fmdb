3a125cf85d5a8a27006287e4600574e65847ab2d
Liam Ilan <snowboardsheep@gmail.com>
2020-07-17 08:35:07 -0700

    Began working on making fmdb functional.

--- src/fmdb/Controller.java
+++ src/fmdb/Controller.java
@@ -23,6 +23,12 @@ public class Controller {
 
     public Button ratingButton;
 
+    public Fmdb db = new Fmdb();
+
+    // false = edit, true = new movie
+    public Boolean movieCreationState = false;
+    public Boolean userCreationState = false;
+
     // GUI methods
     public void openScreen(String path, int width, int height, String title) throws IOException {
         Stage stage = new Stage();
@@ -30,6 +36,60 @@ public class Controller {
         stage.setTitle(title);
         stage.setScene(new Scene(root, width, height));
         stage.show();
+    }
+
+    public void renderUserDropdown() {
+        for (int i = 0; i < db.getPeople().size(); i += 1) {
+            userDropdown.getItems().add(db.getPeople().get(i));
+        }
+
+        userDropdown.getItems().add("-- New User --");
+        userDropdown.getSelectionModel().select(0);
+    }
+
+    public void choseUserItem(int index) {
+        userDropdown.getSelectionModel().select(index);
+
+        if (
+                userDropdown.getSelectionModel().getSelectedIndex() ==
+                        userDropdown.getItems().size() - 1
+        ) {
+            userDropdownButton.setText("New");
+            userCreationState = true;
+        } else {
+            userDropdownButton.setText("Edit");
+            userCreationState = false;
+        }
+    }
+
+    public void renderMovieDropdown() {
+        for (int i = 0; i < db.getMovies().size(); i += 1) {
+            movieDropdown.getItems().add(db.getMovies().get(i));
+        }
+
+        movieDropdown.getItems().add("-- New Movie --");
+        movieDropdown.getSelectionModel().select(0);
+    }
+
+    public void choseMovieItem(int index) {
+        movieDropdown.getSelectionModel().select(index);
+
+        if (
+                movieDropdown.getSelectionModel().getSelectedIndex() ==
+                        movieDropdown.getItems().size() - 1
+        ) {
+            movieDropdownButton.setText("New");
+            movieCreationState = true;
+        } else {
+            movieDropdownButton.setText("Edit");
+            movieCreationState = false;
+        }
+    }
+
+    // events
+    public void initialize() {
+        renderMovieDropdown();
+        renderUserDropdown();
     }
 
     // User pane
